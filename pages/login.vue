78c5ad629f3f094768047281571563189233c72d
piramid <pornchai.tom@gmail.com>
2018-05-05 17:02:55 +0700

    Add files via upload

---
+++ pages/login.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-card class="login-wrapper border border-light">
+    <form class="form-signin" @submit.prevent="login">
+      <h2 class="form-signin-heading">Please sign in</h2>
+      <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+
+    <v-snackbar
+      top
+      v-model="snackbar"
+    >
+      login failed
+      <v-btn flat color="pink" @click.native="snackbar = false">Close</v-btn>
+    </v-snackbar>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: '',
+      snackbar: false,
+    }
+  },
+  methods: {
+    async login () {
+      let res = await this.$http.post('/auth',{email: this.email, password: this.password})
+      if(res.data.ok){
+        this.$router.push('/')
+      } else{
+        this.snackbar=true
+      }
+      //console.log(this.password)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+body {
+  background: #605B56;
+}
+
+.login-wrapper {
+  background: #fff;
+  width: 70%;
+  margin: 12% auto;
+}
+
+.form-signin {
+  max-width: 330px;
+  padding: 10% 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
